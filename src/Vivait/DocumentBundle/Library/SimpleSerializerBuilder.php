eab8a36d86894387c7bdb2b366461395a67b26f3
Lewis Wright <lewis@vivait.co.uk>
2014-05-12 18:34:03 +0100

    Added extra serialization methods for mail merge

--- src/Vivait/DocumentBundle/Library/SimpleSerializerBuilder.php
+++ src/Vivait/DocumentBundle/Library/SimpleSerializerBuilder.php
@@ -2,7 +2,9 @@
 
 namespace Vivait\DocumentBundle\Library;
 
+use JMS\Serializer\Naming\CamelCaseNamingStrategy;
 use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
+use JMS\Serializer\Naming\SerializedNameAnnotationStrategy;
 use JMS\Serializer\SerializerBuilder;
 use Vivait\DocumentBundle\Library\SimpleSerializationVisitor;
 
@@ -13,7 +15,7 @@ class SimpleSerializerBuilder
     {
         return SerializerBuilder::create()
           ->addDefaultHandlers()
-          ->setPropertyNamingStrategy($namingStrategy = new IdenticalPropertyNamingStrategy())
+          ->setPropertyNamingStrategy($namingStrategy = new SerializedNameAnnotationStrategy(new IdenticalPropertyNamingStrategy()))
           ->addDefaultSerializationVisitors()
           ->addDefaultListeners()
           ->setSerializationVisitor('json', new SimpleSerializationVisitor($namingStrategy))
